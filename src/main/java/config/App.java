8f9f43c27ae79a0c1e1495be1c520c834eaaa3af
Pepe Valdivia <jvaldivia@softweb.pe>
2018-04-05 09:35:19 -0500

    Crear coleccion si no existe

--- src/main/java/config/App.java
+++ src/main/java/config/App.java
@@ -1,13 +1,6 @@
 package config;
 
 import daos.BlogDao;
-import io.jsondb.JsonDBTemplate;
-import io.jsondb.crypto.DefaultAESCBCCipher;
-import io.jsondb.crypto.ICipher;
-import java.security.GeneralSecurityException;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import models.Blog;
 
 public class App {
@@ -24,25 +17,4 @@ public class App {
         System.out.println(ex.toString());
       }
   }
-  public void demo(){
-    String dbFilesLocation = "db/";
-    String baseScanPackage = "model";
-    ICipher cipher;
-    try {
-        cipher = new DefaultAESCBCCipher("1r8+24pibarAWgS85/Heeg==");
-        JsonDBTemplate jsonDBTemplate = new JsonDBTemplate(dbFilesLocation, baseScanPackage, cipher);
-        Blog instance = new Blog();
-        //instance.setId("11");
-        instance.setHostname("ec2-54-191-11");
-        instance.setPrivateKey("b87eb02f5dd7e5232d7b0fc30a5015e4");
-        //jsonDBTemplate.createCollection(Blog.class);
-        System.out.println("ANTES");
-        System.out.println(instance);
-        jsonDBTemplate.insert(instance);
-        System.out.println("DESPUES");
-        List<Object> findAll = jsonDBTemplate.findAll("blogs");
-        System.out.println(findAll);
-    } catch (GeneralSecurityException ex) {
-        Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-    }}
-  }
+}
--- src/main/java/daos/BlogDao.java
+++ src/main/java/daos/BlogDao.java
@@ -5,11 +5,21 @@ import java.util.List;
 import models.Blog;
 
 public class BlogDao extends Database{
-  public BlogDao() throws Exception { }
+  public BlogDao() throws Exception {
+    if(this.jsonDBTemplate.collectionExists(Blog.class) == false){
+      this.jsonDBTemplate.createCollection(Blog.class);
+    }
+  }
     
   public void insertar(Blog instance) throws Exception{
     try {
-      this.getJsonDBTemplate().insert(instance);
+      System.out.println("ANTES");
+      System.out.println(instance);
+      this.getJsonDBTemplate().upsert(instance);
+      //this.getJsonDBTemplate().save(instance, Blog.class);
+      System.out.println("DESPUES");
+      System.out.println(instance);
+      System.out.println("++++++++++++++++++++++++++++");
     } catch (Exception ex) {
       throw ex;    
     }
